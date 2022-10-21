a24623c4442b4a973d8cbe68720a6598a38d8114
github-action-benchmark <github@users.noreply.github.com>
2022-10-21 17:55:14 +0000

    add Benchmark (cargo) benchmark result for f444001c6dd455bb4dacb57215ccaa2d10ab6603

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1666374914934,
+  "repoUrl": "https://github.com/psvri/aoc2020",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "psvrishabh@gmail.com",
+            "name": "psvri",
+            "username": "psvri"
+          },
+          "committer": {
+            "email": "psvrishabh@gmail.com",
+            "name": "psvri",
+            "username": "psvri"
+          },
+          "distinct": true,
+          "id": "f444001c6dd455bb4dacb57215ccaa2d10ab6603",
+          "message": "Workflow fix benchmark",
+          "timestamp": "2022-10-21T23:21:27+05:30",
+          "tree_id": "5c659f0ae5f786a7ecbe66982fd1c3e085d01ebe",
+          "url": "https://github.com/psvri/aoc2020/commit/f444001c6dd455bb4dacb57215ccaa2d10ab6603"
+        },
+        "date": 1666374914532,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "day 1",
+            "value": 763272,
+            "range": "± 2336",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "day seq",
+            "value": 778855,
+            "range": "± 5063",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
